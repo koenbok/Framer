0e04a1cf228e6a860491008c26914e97d364f159
Koen Bok <koen@madebysofa.com>
2014-05-03 14:38:18 +0200

    Add easy visual tests to Cactus project

--- extras/CactusFramer/plugins/default.py
+++ extras/CactusFramer/plugins/default.py
@@ -23,7 +23,23 @@ def run(command):
     if stdout: logger.info(stdout)
     if stderr: logger.warning(stderr)
 
+EXTRA_CONTEXT = {}
+
 def preBuild(site):
+
+    global EXTRA_CONTEXT
+
     run('find %s -name "*.scss" -not -name "_*" -exec scss -C --update {} \;' % pipes.quote(site.static_path))
     run('find %s -name "*.sass" -not -name "_*" -exec scss -C --update {} \;' % pipes.quote(site.static_path))
     run('find %s -name "*.coffee" -exec coffee -c {} \;' % pipes.quote(site.static_path))
+
+    EXTRA_CONTEXT["testScripts"] = []
+
+    for fileName in os.listdir(os.path.join(site.static_path, "tests")):
+        if fileName.endswith(".coffee"):
+            EXTRA_CONTEXT["testScripts"].append(fileName)
+
+def preBuildPage(site, context, data):
+    context.update(EXTRA_CONTEXT)
+    return context, data
+
