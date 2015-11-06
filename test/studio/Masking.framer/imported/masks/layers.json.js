a22ecf0806746c8c65be5774370823be1574600e
Benjamin den Boer <benjamindenboer@hotmail.com>
2015-11-06 14:30:25 +0100

    Update Mask Testing Environment

--- test/studio/Masking.framer/imported/masks/layers.json.js
+++ test/studio/Masking.framer/imported/masks/layers.json.js
@@ -2,39 +2,39 @@ window.__imported__ = window.__imported__ || {};
 window.__imported__["masks/layers.json.js"] = [
   {
     "maskFrame" : {
-      "y" : 132,
-      "x" : 50,
+      "y" : 612,
+      "x" : 772,
       "width" : 300,
       "height" : 300
     },
-    "id" : "35D27058-CA6A-4E69-A554-199C118C4459",
+    "id" : "A0FD1F5A-DF42-4D36-B99C-64AF713A4916",
     "visible" : true,
     "children" : [
       {
         "maskFrame" : {
-          "y" : 282,
-          "x" : 200,
+          "y" : 762,
+          "x" : 922,
           "width" : 150,
           "height" : 150
         },
-        "id" : "03B6E603-BF6F-4E8E-B115-A1100B91E662",
+        "id" : "E295FDF2-8CFA-4D56-B282-EEE877F81682",
         "visible" : true,
         "children" : [
 
         ],
         "image" : {
-          "path" : "images\/Layer-mask-03B6E603-BF6F-4E8E-B115-A1100B91E662.png",
+          "path" : "images\/Layer-mask-E295FDF2-8CFA-4D56-B282-EEE877F81682.png",
           "frame" : {
-            "y" : 282,
-            "x" : 200,
+            "y" : 662,
+            "x" : 822,
             "width" : 300,
             "height" : 300
           }
         },
         "imageType" : "png",
         "layerFrame" : {
-          "y" : 282,
-          "x" : 200,
+          "y" : 662,
+          "x" : 822,
           "width" : 300,
           "height" : 300
         },
@@ -42,94 +42,10 @@ window.__imported__["masks/layers.json.js"] = [
       }
     ],
     "image" : {
-      "path" : "images\/Layer-parentA-35D27058-CA6A-4E69-A554-199C118C4459.png",
-      "frame" : {
-        "y" : 132,
-        "x" : 50,
-        "width" : 300,
-        "height" : 300
-      }
-    },
-    "imageType" : "png",
-    "layerFrame" : {
-      "y" : 132,
-      "x" : 50,
-      "width" : 300,
-      "height" : 300
-    },
-    "name" : "parentA"
-  },
-  {
-    "maskFrame" : null,
-    "id" : "982C4A93-985D-4A14-9FB9-4B5E979C2226",
-    "visible" : true,
-    "children" : [
-
-    ],
-    "image" : {
-      "path" : "images\/Layer-label-982C4A93-985D-4A14-9FB9-4B5E979C2226.png",
-      "frame" : {
-        "y" : 61,
-        "x" : 70,
-        "width" : 262,
-        "height" : 48
-      }
-    },
-    "imageType" : "png",
-    "layerFrame" : {
-      "y" : 61,
-      "x" : 70,
-      "width" : 262,
-      "height" : 48
-    },
-    "name" : "label"
-  },
-  {
-    "maskFrame" : {
-      "y" : 612,
-      "x" : 50,
-      "width" : 300,
-      "height" : 300
-    },
-    "id" : "BEFD9E38-0925-487B-AAFE-2FBAFA397AF3",
-    "visible" : true,
-    "children" : [
-      {
-        "maskFrame" : {
-          "y" : 687,
-          "x" : 125,
-          "width" : 200,
-          "height" : 200
-        },
-        "id" : "BE2E06E1-C704-4741-8D34-610CCEFD41B7",
-        "visible" : true,
-        "children" : [
-
-        ],
-        "image" : {
-          "path" : "images\/Layer-mask-BE2E06E1-C704-4741-8D34-610CCEFD41B7.png",
-          "frame" : {
-            "y" : 687,
-            "x" : 125,
-            "width" : 200,
-            "height" : 200
-          }
-        },
-        "imageType" : "png",
-        "layerFrame" : {
-          "y" : 687,
-          "x" : 125,
-          "width" : 200,
-          "height" : 200
-        },
-        "name" : "mask"
-      }
-    ],
-    "image" : {
-      "path" : "images\/Layer-parentA-BEFD9E38-0925-487B-AAFE-2FBAFA397AF3.png",
+      "path" : "images\/Layer-parentF-A0FD1F5A-DF42-4D36-B99C-64AF713A4916.png",
       "frame" : {
         "y" : 612,
-        "x" : 50,
+        "x" : 772,
         "width" : 300,
         "height" : 300
       }
@@ -137,36 +53,11 @@ window.__imported__["masks/layers.json.js"] = [
     "imageType" : "png",
     "layerFrame" : {
       "y" : 612,
-      "x" : 50,
+      "x" : 772,
       "width" : 300,
       "height" : 300
     },
-    "name" : "parentA"
-  },
-  {
-    "maskFrame" : null,
-    "id" : "433500E8-13BC-4AF8-B65C-748ACE493CE5",
-    "visible" : true,
-    "children" : [
-
-    ],
-    "image" : {
-      "path" : "images\/Layer-label-433500E8-13BC-4AF8-B65C-748ACE493CE5.png",
-      "frame" : {
-        "y" : 541,
-        "x" : 76,
-        "width" : 249,
-        "height" : 48
-      }
-    },
-    "imageType" : "png",
-    "layerFrame" : {
-      "y" : 541,
-      "x" : 76,
-      "width" : 249,
-      "height" : 48
-    },
-    "name" : "label"
+    "name" : "parentF"
   },
   {
     "maskFrame" : {
@@ -210,7 +101,7 @@ window.__imported__["masks/layers.json.js"] = [
       }
     ],
     "image" : {
-      "path" : "images\/Layer-parentA-14D768D1-2821-4DEE-8C46-16A8A0904F73.png",
+      "path" : "images\/Layer-parentE-14D768D1-2821-4DEE-8C46-16A8A0904F73.png",
       "frame" : {
         "y" : 612,
         "x" : 412,
@@ -225,32 +116,125 @@ window.__imported__["masks/layers.json.js"] = [
       "width" : 300,
       "height" : 300
     },
-    "name" : "parentA"
+    "name" : "parentE"
   },
   {
-    "maskFrame" : null,
-    "id" : "07CA3415-BE33-4CB5-9FA2-F571DEBB9A07",
+    "maskFrame" : {
+      "y" : 612,
+      "x" : 50,
+      "width" : 300,
+      "height" : 301
+    },
+    "id" : "BEFD9E38-0925-487B-AAFE-2FBAFA397AF3",
     "visible" : true,
     "children" : [
+      {
+        "maskFrame" : {
+          "y" : 687,
+          "x" : 125,
+          "width" : 200,
+          "height" : 201
+        },
+        "id" : "BE2E06E1-C704-4741-8D34-610CCEFD41B7",
+        "visible" : true,
+        "children" : [
 
+        ],
+        "image" : {
+          "path" : "images\/Layer-mask-BE2E06E1-C704-4741-8D34-610CCEFD41B7.png",
+          "frame" : {
+            "y" : 687,
+            "x" : 125,
+            "width" : 200,
+            "height" : 201
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 687,
+          "x" : 125,
+          "width" : 200,
+          "height" : 201
+        },
+        "name" : "mask"
+      }
     ],
     "image" : {
-      "path" : "images\/Layer-label-07CA3415-BE33-4CB5-9FA2-F571DEBB9A07.png",
+      "path" : "images\/Layer-parentD-BEFD9E38-0925-487B-AAFE-2FBAFA397AF3.png",
       "frame" : {
-        "y" : 541,
-        "x" : 462,
-        "width" : 202,
-        "height" : 48
+        "y" : 612,
+        "x" : 50,
+        "width" : 300,
+        "height" : 301
       }
     },
     "imageType" : "png",
     "layerFrame" : {
-      "y" : 541,
-      "x" : 462,
-      "width" : 202,
-      "height" : 48
+      "y" : 612,
+      "x" : 50,
+      "width" : 300,
+      "height" : 301
     },
-    "name" : "label"
+    "name" : "parentD"
+  },
+  {
+    "maskFrame" : {
+      "y" : 132,
+      "x" : 770,
+      "width" : 300,
+      "height" : 300
+    },
+    "id" : "7A84AEE4-02A6-4CE9-A301-DC6F3A5CE77D",
+    "visible" : true,
+    "children" : [
+      {
+        "maskFrame" : {
+          "y" : 207,
+          "x" : 845,
+          "width" : 150,
+          "height" : 150
+        },
+        "id" : "78A4DB47-C8A9-49C1-B2E4-169B042EFD8D",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-mask-78A4DB47-C8A9-49C1-B2E4-169B042EFD8D.png",
+          "frame" : {
+            "y" : 207,
+            "x" : 845,
+            "width" : 225,
+            "height" : 225
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 207,
+          "x" : 845,
+          "width" : 225,
+          "height" : 225
+        },
+        "name" : "mask"
+      }
+    ],
+    "image" : {
+      "path" : "images\/Layer-parentC-7A84AEE4-02A6-4CE9-A301-DC6F3A5CE77D.png",
+      "frame" : {
+        "y" : 132,
+        "x" : 770,
+        "width" : 300,
+        "height" : 300
+      }
+    },
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 132,
+      "x" : 770,
+      "width" : 300,
+      "height" : 300
+    },
+    "name" : "parentC"
   },
   {
     "maskFrame" : {
@@ -312,65 +296,40 @@ window.__imported__["masks/layers.json.js"] = [
     "name" : "parentB"
   },
   {
-    "maskFrame" : null,
-    "id" : "C6DDFA1D-1780-41FA-99E8-010C7A3E854A",
-    "visible" : true,
-    "children" : [
-
-    ],
-    "image" : {
-      "path" : "images\/Layer-label-C6DDFA1D-1780-41FA-99E8-010C7A3E854A.png",
-      "frame" : {
-        "y" : 61,
-        "x" : 430,
-        "width" : 262,
-        "height" : 48
-      }
-    },
-    "imageType" : "png",
-    "layerFrame" : {
-      "y" : 61,
-      "x" : 430,
-      "width" : 262,
-      "height" : 48
-    },
-    "name" : "label"
-  },
-  {
     "maskFrame" : {
-      "y" : 612,
-      "x" : 772,
+      "y" : 132,
+      "x" : 50,
       "width" : 300,
       "height" : 300
     },
-    "id" : "A0FD1F5A-DF42-4D36-B99C-64AF713A4916",
+    "id" : "35D27058-CA6A-4E69-A554-199C118C4459",
     "visible" : true,
     "children" : [
       {
         "maskFrame" : {
-          "y" : 762,
-          "x" : 922,
+          "y" : 282,
+          "x" : 200,
           "width" : 150,
           "height" : 150
         },
-        "id" : "E295FDF2-8CFA-4D56-B282-EEE877F81682",
+        "id" : "03B6E603-BF6F-4E8E-B115-A1100B91E662",
         "visible" : true,
         "children" : [
 
         ],
         "image" : {
-          "path" : "images\/Layer-mask-E295FDF2-8CFA-4D56-B282-EEE877F81682.png",
+          "path" : "images\/Layer-mask-03B6E603-BF6F-4E8E-B115-A1100B91E662.png",
           "frame" : {
-            "y" : 662,
-            "x" : 822,
+            "y" : 282,
+            "x" : 200,
             "width" : 300,
             "height" : 300
           }
         },
         "imageType" : "png",
         "layerFrame" : {
-          "y" : 662,
-          "x" : 822,
+          "y" : 282,
+          "x" : 200,
           "width" : 300,
           "height" : 300
         },
@@ -378,18 +337,18 @@ window.__imported__["masks/layers.json.js"] = [
       }
     ],
     "image" : {
-      "path" : "images\/Layer-parentA-A0FD1F5A-DF42-4D36-B99C-64AF713A4916.png",
+      "path" : "images\/Layer-parentA-35D27058-CA6A-4E69-A554-199C118C4459.png",
       "frame" : {
-        "y" : 612,
-        "x" : 772,
+        "y" : 132,
+        "x" : 50,
         "width" : 300,
         "height" : 300
       }
     },
     "imageType" : "png",
     "layerFrame" : {
-      "y" : 612,
-      "x" : 772,
+      "y" : 132,
+      "x" : 50,
       "width" : 300,
       "height" : 300
     },
@@ -397,111 +356,27 @@ window.__imported__["masks/layers.json.js"] = [
   },
   {
     "maskFrame" : null,
-    "id" : "7100928B-6344-4BCE-8484-11B32446A96F",
+    "id" : "5D3F55C0-2333-4FB4-8368-FAC412C6BC72",
     "visible" : true,
     "children" : [
 
     ],
     "image" : {
-      "path" : "images\/Layer-label-7100928B-6344-4BCE-8484-11B32446A96F.png",
-      "frame" : {
-        "y" : 541,
-        "x" : 766,
-        "width" : 314,
-        "height" : 49
-      }
-    },
-    "imageType" : "png",
-    "layerFrame" : {
-      "y" : 541,
-      "x" : 766,
-      "width" : 314,
-      "height" : 49
-    },
-    "name" : "label"
-  },
-  {
-    "maskFrame" : {
-      "y" : 132,
-      "x" : 770,
-      "width" : 300,
-      "height" : 300
-    },
-    "id" : "7A84AEE4-02A6-4CE9-A301-DC6F3A5CE77D",
-    "visible" : true,
-    "children" : [
-      {
-        "maskFrame" : {
-          "y" : 207,
-          "x" : 845,
-          "width" : 150,
-          "height" : 150
-        },
-        "id" : "78A4DB47-C8A9-49C1-B2E4-169B042EFD8D",
-        "visible" : true,
-        "children" : [
-
-        ],
-        "image" : {
-          "path" : "images\/Layer-mask-78A4DB47-C8A9-49C1-B2E4-169B042EFD8D.png",
-          "frame" : {
-            "y" : 207,
-            "x" : 845,
-            "width" : 225,
-            "height" : 225
-          }
-        },
-        "imageType" : "png",
-        "layerFrame" : {
-          "y" : 207,
-          "x" : 845,
-          "width" : 225,
-          "height" : 225
-        },
-        "name" : "mask"
-      }
-    ],
-    "image" : {
-      "path" : "images\/Layer-parentB-7A84AEE4-02A6-4CE9-A301-DC6F3A5CE77D.png",
-      "frame" : {
-        "y" : 132,
-        "x" : 770,
-        "width" : 300,
-        "height" : 300
-      }
-    },
-    "imageType" : "png",
-    "layerFrame" : {
-      "y" : 132,
-      "x" : 770,
-      "width" : 300,
-      "height" : 300
-    },
-    "name" : "parentB"
-  },
-  {
-    "maskFrame" : null,
-    "id" : "FB756A0F-D3EF-4529-A91D-A78561E75437",
-    "visible" : true,
-    "children" : [
-
-    ],
-    "image" : {
-      "path" : "images\/Layer-label-FB756A0F-D3EF-4529-A91D-A78561E75437.png",
+      "path" : "images\/Layer-labels-5D3F55C0-2333-4FB4-8368-FAC412C6BC72.png",
       "frame" : {
         "y" : 61,
-        "x" : 784,
-        "width" : 273,
-        "height" : 49
+        "x" : 70,
+        "width" : 1010,
+        "height" : 529
       }
     },
     "imageType" : "png",
     "layerFrame" : {
       "y" : 61,
-      "x" : 784,
-      "width" : 273,
-      "height" : 49
+      "x" : 70,
+      "width" : 1010,
+      "height" : 529
     },
-    "name" : "label"
+    "name" : "labels"
   }
 ]
