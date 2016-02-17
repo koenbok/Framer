koenbok/Framer

commit 74b8b3b75a717c453b471d8ea2bca971284c257d

Fix overdrag disabled

framer/LayerDraggable.coffee
@@ -182,6 +182,15 @@ class exports.LayerDraggable extends BaseClass
 			y: touchEvent.clientY
 			t: Date.now() # We don't use timeStamp because it's different on Chrome/Safari
 
+		# Disable constraints drag if overdrag is disabled
+		if @overdrag is false
+			# TODO: We still need to account for the cursor offset here
+			frame = Utils.convertFrameToContext(@constraints, @layer, true, false)
+			return if event.point.x < Utils.frameGetMinX(frame) 
+			return if event.point.x > Utils.frameGetMaxX(frame)
+			return if event.point.y < Utils.frameGetMinY(frame) 
+			return if event.point.y > Utils.frameGetMaxY(frame)
+	
 		point = _.clone(@_point)
 
 		scaleX = (1 / @layer.canvasScaleX() * @layer.scale * @layer.scaleX)

commit e2e7852d4d2e9be59e3efd89ad9aabbb69485c49

Fix draggable and slider component
The fix I added in https://github.com/koenbok/Framer/commit/74b8b3b75a717c453b471d8ea2bca971284c257d broke this, and it seems not to be needed anymore.

framer/LayerDraggable.coffee
@@ -190,16 +190,6 @@ class exports.LayerDraggable extends BaseClass
 			y: touchEvent.clientY
 			t: Date.now() # We don't use timeStamp because it's different on Chrome/Safari
 
-		# Disable constraints drag if overdrag is disabled
-		if @overdrag is false
-			# TODO: We still need to account for the cursor offset here
-			frame = Utils.convertFrameToContext(@constraints, @layer, true, false)
-			return if event.point.x < Utils.frameGetMinX(frame)
-			return if event.point.x > Utils.frameGetMaxX(frame)
-			return if event.point.y < Utils.frameGetMinY(frame)
-			return if event.point.y > Utils.frameGetMaxY(frame)
-
-
 		point = _.clone(@_point)
 
 		scaleX = (1 / @layer.canvasScaleX() * @layer.scale * @layer.scaleX)