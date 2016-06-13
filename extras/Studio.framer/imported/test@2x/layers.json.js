a3eddaa76c1a05b0704e17ef715363c4b2a1fc99
Eelco Lempsink <eelco@motif.co>
2016-06-13 11:15:12 +0200

    Improve PageComponent.wrap()

---
+++ extras/Studio.framer/imported/test@2x/layers.json.js
@@ -0,0 +1,29 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["test@2x/layers.json.js"] = [
+	{
+		"objectId": "41200C5F-397F-4ED6-BA30-E08AE1DD7F6C",
+		"kind": "group",
+		"name": "main",
+		"maskFrame": null,
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 375,
+			"height": 1000
+		},
+		"visible": true,
+		"metadata": {
+			"opacity": 1
+		},
+		"image": {
+			"path": "images/Layer-main-ndeymdbd.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 375,
+				"height": 1000
+			}
+		},
+		"children": []
+	}
+]
