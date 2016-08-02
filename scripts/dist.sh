fd32c4ce570b1a89fc66ef63629963b7815c7a30
Niels van Hoorn <nvh@nvh.io>
2016-08-02 14:21:09 +0200

    Get distributions from devmate

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+DEVMATE_FEED="https://updates.devmate.com/co.motif.framer.generator.xml"
 
-GENERATOR_HOST="http://framergenerator-update.s3-website-us-east-1.amazonaws.com"
-GENERATOR_LATEST=$GENERATOR_HOST/`curl -s $GENERATOR_HOST/latest.txt`
+# GENERATOR_HOST="http://framergenerator-update.s3-website-us-east-1.amazonaws.com"
+# GENERATOR_LATEST=$GENERATOR_HOST/`curl -s $GENERATOR_HOST/latest.txt`
 
 # Clean up previous builds
 rm -f build/Framer.zip
@@ -18,15 +19,17 @@ cp build/framer.js.map build/Framer/Project/framer/framer.js.map
 
 # Copy over extra images
 IMAGES_DIR=build/Framer/Project/framer/images
-mkdir -p "$IMAGES_DIR" 
+mkdir -p "$IMAGES_DIR"
 cp -R extras/cursor-images/* "$IMAGES_DIR"
 cp -R extras/preloader-images/* "$IMAGES_DIR"
 
 # Download the latest generator
 cd build/Framer
-curl "${GENERATOR_LATEST// /%20}" -o "generator.tgz"
-tar zxf "./generator.tgz"
-rm "./generator.tgz"
+curl --silent --location $DEVMATE_FEED --output devmate.xml
+xmllint --xpath "string(//item[1]/enclosure/@url)" devmate.xml | xargs curl --location --output generator.zip
+unzip "./generator.zip"
+rm "./devmate.xml"
+rm "./generator.zip"
 cd -
 
 # Clean up and zip the result
