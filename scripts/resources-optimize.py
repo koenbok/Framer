e33975095059cf4befa9e59d6b5b398360d481bc
Eelco Lempsink <eelco@motif.co>
2016-09-29 16:27:48 +0200

    Use lossless JPEG2000 files for the new Apple Watches

--- scripts/resources-optimize.py
+++ scripts/resources-optimize.py
@@ -14,12 +14,12 @@ jp2_quality = 16
 webp_quality = 90
 
 
-#os.system("rm -Rf '%s'" % path)
-#os.system("cp -Rf '%s' '%s'" % (os.path.join("extras", "DeviceResources"), path))
+os.system("rm -Rf '%s'" % path)
+os.system("cp -Rf '%s' '%s'" % (os.path.join("extras", "DeviceResources"), path))
 os.chdir(path)
 
 for fileName in os.listdir("."):
-	if fileName.endswith(".png") and (fileName.startswith("apple-watch-series") or fileName.startswith("apple-watch-nike")):
+        if fileName.endswith(".png"):
 		print fileName
 		os.system("sips -s format jp2 %s -s formatOptions %s --out %s" % (fileName, jp2_quality, fileName.replace(".png", ".jp2")))
 		os.system("cwebp -q %s '%s' -o '%s'" % (webp_quality, fileName, fileName.replace(".png", ".webp")))
