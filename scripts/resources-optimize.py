f8391b4f995e027aec3d417f8e8563745259a8e5
Eelco Lempsink <eelco@motif.co>
2016-11-22 09:49:25 +0100

    Re-doing encoding of Apple Watch images on lossless quality

--- scripts/resources-optimize.py
+++ scripts/resources-optimize.py
@@ -10,7 +10,7 @@ brew install webp
 """
 
 path = os.path.join("extras", "resources.framerjs.com", "static", "DeviceResources")
-jp2_quality = 16
+jp2_quality = 100
 webp_quality = 90
 
 
@@ -19,7 +19,7 @@ webp_quality = 90
 os.chdir(path)
 
 for fileName in os.listdir("."):
-        if fileName.endswith(".png"):
+        if fileName.startswith("apple-watch") and fileName.endswith(".png"):
 		print fileName
 		os.system("sips -s format jp2 %s -s formatOptions %s --out %s" % (fileName, jp2_quality, fileName.replace(".png", ".jp2")))
 #		os.system("cwebp -q %s '%s' -o '%s'" % (webp_quality, fileName, fileName.replace(".png", ".webp")))
