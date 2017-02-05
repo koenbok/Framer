bf7e89e64091867b9d1f6523960f44322b07ce09
Koen Bok <koen@motif.co>
2017-02-05 14:22:15 +0100

    Restore tests

---
+++ tests/EventEmitter.test.ts
@@ -0,0 +1,185 @@
+import * as assert from "assert"
+
+import {EventEmitter} from "EventEmitter"
+
+const counter = () => {
+	let count = 0
+	return {
+		value: () => count,
+		add: () => count++
+	}
+}
+
+
+it("should emit", () => {
+
+	const em = new EventEmitter<"test">()
+	const count = counter()
+
+	em.on("test", count.add)
+
+	em.emit("test")
+	assert.equal(count.value(), 1)
+
+	em.emit("test")
+	assert.equal(count.value(), 2)
+
+	em.emit("test")
+	assert.equal(count.value(), 3)
+})
+
+it("should remove by handler", () => {
+
+	const em = new EventEmitter<"testA" | "testB">()
+	const countA = counter()
+	const countB = counter()
+
+	em.on("testA", countA.add)
+	em.on("testB", countB.add)
+
+	em.emit("testA")
+	em.emit("testB")
+	assert.equal(countA.value(), 1)
+	assert.equal(countB.value(), 1)
+
+	em.removeEventListeners("testA", countA.add)
+
+	em.emit("testA")
+	em.emit("testB")
+	assert.equal(countA.value(), 1)
+	assert.equal(countB.value(), 2)
+
+})
+
+
+it("should remove by name", () => {
+
+	const em = new EventEmitter<"testA" | "testB">()
+	const countA = counter()
+	const countB = counter()
+
+	em.on("testA", countA.add)
+	em.on("testB", countB.add)
+
+	em.emit("testA")
+	em.emit("testB")
+	assert.equal(countA.value(), 1)
+	assert.equal(countB.value(), 1)
+
+	em.removeEventListeners("testA")
+
+	em.emit("testA")
+	em.emit("testB")
+	assert.equal(countA.value(), 1)
+	assert.equal(countB.value(), 2)
+
+})
+
+
+it("should remove all", () => {
+
+	const em = new EventEmitter<"testA" | "testB">()
+	const countA = counter()
+	const countB = counter()
+
+	em.on("testA", countA.add)
+	em.on("testB", countB.add)
+
+	em.emit("testA")
+	em.emit("testB")
+	assert.equal(countA.value(), 1)
+	assert.equal(countB.value(), 1)
+
+	em.removeEventListeners()
+
+	em.emit("testA")
+	em.emit("testB")
+	assert.equal(countA.value(), 1)
+	assert.equal(countB.value(), 1)
+
+})
+
+it("should once", () => {
+
+	const em = new EventEmitter<"test">()
+	const count = counter()
+
+	em.once("test", count.add)
+
+	em.emit("test")
+	assert.equal(count.value(), 1)
+
+	em.emit("test")
+	assert.equal(count.value(), 1)
+
+	em.emit("test")
+	assert.equal(count.value(), 1)
+})
+
+it("should schuedlue", () => {
+
+	const em = new EventEmitter<"test">()
+	const count = counter()
+
+	assert.equal(em.schedule("test", count.add), true)
+	assert.equal(em.schedule("test", count.add), false)
+	assert.equal(em.schedule("test", count.add), false)
+
+	em.emit("test")
+	assert.equal(count.value(), 1)
+
+})
+
+it("should wrap", () => {
+
+	let wrapCounter = 0
+
+	// Create a custom class that wraps handler functions
+	class EventEmitterTest<EventName> extends EventEmitter<EventName> {
+		wrapEventListener(eventName: EventName, handler: Function) {
+			return () => {
+				wrapCounter++
+				handler()
+			}
+		}
+	}
+
+	const em = new EventEmitterTest<"test">()
+	const count = counter()
+
+	em.on("test", count.add)
+
+	em.emit("test")
+	assert.equal(count.value(), 1)
+	assert.equal(wrapCounter, 1)
+
+	em.emit("test")
+	assert.equal(count.value(), 2)
+	assert.equal(wrapCounter, 2)
+
+	em.removeEventListeners("test", count.add)
+
+	em.emit("test")
+	assert.equal(count.value(), 2)
+	assert.equal(wrapCounter, 2)
+
+})
+
+it("should once per emitter", () => {
+
+	const em1 = new EventEmitter<"test">()
+	const em2 = new EventEmitter<"test">()
+
+	const count = counter()
+	em1.once("test", count.add)
+	em2.on("test", count.add)
+
+	em1.emit("test")
+	em2.emit("test")
+	assert.equal(count.value(), 2)
+
+	em1.emit("test")
+	em2.emit("test")
+	assert.equal(count.value(), 3)
+
+})
---
+++ tests/Layer.test.ts
@@ -0,0 +1,64 @@
+import * as assert from "assert"
+
+import {Layer} from "Layer"
+
+
+it("should have defaults", () => {
+
+	let layer = new Layer()
+
+	assert.equal(layer.x, 0)
+	assert.equal(layer.y, 0)
+	assert.equal(layer.width, 200)
+	assert.equal(layer.height, 200)
+})
+
+it("should take defaults", () => {
+
+	let layer = new Layer({x: 500, y: 500})
+
+	assert.equal(layer.x, 500)
+	assert.equal(layer.y, 500)
+})
+
+it("should set dirty", () => {
+
+	let layer = new Layer()
+
+	layer.x = 500
+	layer.y = 500
+
+	assert.equal(layer.isDirty(), true)
+	assert.deepEqual(layer.dirtyValues(), {x: 500, y: 500})
+
+})
+
+it("should flush", () => {
+
+	let layer = new Layer()
+
+	layer.x = 500
+	layer.y = 500
+
+	assert.equal(layer.isDirty(), true)
+	assert.deepEqual(layer.dirtyValues(), {x: 500, y: 500})
+
+	layer.flush()
+
+	assert.equal(layer.isDirty(), false)
+	assert.deepEqual(layer.dirtyValues(), {})
+
+})
+
+it("should set parent", () => {
+
+	let layerA = new Layer()
+	let layerB = new Layer()
+
+	assert.equal(layerA.parent, null)
+
+	layerA.parent = layerB
+	assert.equal(layerA.parent, layerB)
+	assert.deepEqual(layerB.children, [layerA])
+
+})
