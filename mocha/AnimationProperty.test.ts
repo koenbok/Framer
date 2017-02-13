d41b0b783bfd032afb0370b1a1489755d63d7e02
Koen Bok <koen@motif.co>
2017-02-13 14:26:07 +0100

    Test for property animation

---
+++ mocha/AnimationProperty.test.ts
@@ -0,0 +1,35 @@
+import {expect} from "chai"
+import {isolated} from "./TestUtils"
+
+import {Layer} from "Layer"
+import {Curve} from "Curve"
+import {AnimationProperty} from "AnimationProperty"
+
+isolated("it should set the right value", (context, done) => {
+
+	context.renderer.loop.pause = true
+
+	const layer = new Layer()
+	const ap = new AnimationProperty(
+		context.renderer.loop, layer, "x",
+		100, 160, Curve.linear(1))
+
+	expect(ap.running).to.be.false
+	ap.start()
+	expect(ap.running).to.be.true
+
+	context.renderer.loop.next()
+	expect(layer.x).to.be.closeTo(101, 0.001)
+
+	context.renderer.loop.next()
+	expect(layer.x).to.be.closeTo(102, 0.001)
+
+	context.renderer.loop.next(57)
+	expect(layer.x).to.be.closeTo(159, 0.001)
+
+	context.renderer.loop.next() // Done
+	expect(layer.x).to.equal(160)
+
+	done()
+
+})
